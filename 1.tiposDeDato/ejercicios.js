ee07a47cf45f551af3041dc085548c3827586c6c
maycol <maycoledgar04@gmail.com>
2022-10-19 18:10:52 -0500

    tarea de investigación

---
+++ 1.tiposDeDato/ejercicios.js
@@ -0,0 +1,21 @@
+//averiguar metedo de javascript
+//que me permite saber que tipo de dato 
+//tengo
+
+/*typeof 
+    typeof El operador typeof devuelve una cadena que indica el tipo del operando sin evaluarlo. 
+    operando es la cadena, variable, palabra clave u objeto para el que se devolverá su tipo. Los paréntesis son opcionales.
+    Para las palabras clave true y null, el operador typeof devuelve los siguientes resultados:
+    que me permite saber que tipo de dato tengo*/
+
+    var tipo=typeof("hola")
+    console.log(tipo)
+
+    var tipo=typeof(9)
+    console.log(tipo)
+    
+
+    //typeof true === 'boolean'
+    //typeof null === 'object'
+    //typeof 62 === 'number'
+    //typeof 'Hola mundo' === 'string'
--- 2.variables/ejercicios.js
+++ 2.variables/ejercicios.js
@@ -1,5 +1,40 @@
 //averiguar las diferencias entra let y var
 // la cocina del codigo
+/*VAR la cual define una variable 
+  global o local en una función sin importar el ámbito del bloque.*/
+  
+  var tipo=("hola")
+  console.log(tipo)
+ 
+ /*let te permite declarar variables limitando su alcance 
+ al bloque, declaración, o expresión donde se está usando.*/
+
+   let tipo2=(9)
+   console.log(tipo2)
+
+ 
+
+ //la cocina del codigo//
+    /*https://www.youtube.com/c/LaCocinadelC%C3%B3digo*/
+    
 
 //hosting, scope
 //contexto gobal y local
+
+/*Scope
+  El scope define el alcance o la visibilidad de las variables en algún punto de la ejecución del código.
+  En los navegadores existe un scope global, que es el objeto window; en Node.js es el objeto global.*/
+
+  /*Hoisting
+  Hoisting es el comportamiento por defecto de JavaScript en el que la declaración de variables y funciones se mueve automáticamente al principio del scope (ya sea el principio del archivo, la función o el bloque). Veamos un ejemplo:
+  hello*/
+
+  function hello() {
+    console.log("Hola")
+    }
+  
+  //contexto global y local//
+    /*Una variable global es aquella que está disponible a lo largo del script actual, es decir, dentro
+    de todo el código JavaScript que se está escribiendo.*/
+    /*Las variables locales sólo resultan visibles y disponibles dentro de la función en la que están definidas.*/
+  
