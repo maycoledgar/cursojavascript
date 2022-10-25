d70199a36fdab3497531d2f8679555de6258a890
maycol <maycoledgar04@gmail.com>
2022-10-25 08:58:52 -0500

    estructuradesición

---
+++ 5.estructuraDesicion/ejercicios.js
@@ -0,0 +1,7 @@
+// ESTRUCTURA DESICIÓN:
+ 
+// CONCEPTO:
+// Una estructura de decisión permite que la ejecución de un algoritmo tome distintos caminos, que llevan a distintos resultados, 
+// dependiendo de la condición o condiciones presentes en la estructura de decisión.
+
+
---
+++ 5.estructuraDesicion/index.js
@@ -0,0 +1,37 @@
+// EJEMPLO1
+// Estructura de Decisión Compuesta:
+
+Var
+ Real: N1, N2, N3, P
+Inicio
+// Escribir “Ingrese las tres calificaciones”
+Leer 
+N1, N2, N3
+ P (N1+ N2 +N3)/3
+ Si (P >= 3) 
+entonces
+"Aprueba el Curso"
+//Sino Escribir 
+ "Desaprueba el Curso"
+ //Fin si
+
+
+
+
+//  EJEMPLO2:
+// ESTRUCTURA desición simple:
+
+// Algoritmo HallarEdad
+Var
+ Entero: Edad
+Inicio
+// Escribir “Digite su edad”
+// Leer Edad
+Si (Edad >= 18) 
+entonces
+//  Escribir: 
+"La persona de edad ", Edad, " es mayor de Edad"
+//  Fin si
+//  Escribir: 
+"Gracias por usar este programa"
+Fin
