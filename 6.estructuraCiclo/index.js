04affea3e1e677f92618a2ecfd399afe4ae3eec0
maycol <maycoledgar04@gmail.com>
2022-11-07 12:28:07 -0500

    trabajo de investigacion de '7.funciones'

--- 6.estructuraCiclo/index.js
+++ 6.estructuraCiclo/index.js
@@ -1,8 +1,53 @@
 // EJEMPLO DE FOR:
 
-#include <stdio.h>
+
 for(let i=2;i<=10;i+=2){
-    console.log(i)
+console.log(i)
 }
 
-// EJEMPLO DE DO-WHILE:
+for (let inicial=2; inicial<=20; inicial+=2){
+console.log(inicial)
+}
+
+for (let i=1; i<=20; i++){
+   if (i%2==0){
+    console.log(i)
+   }else{
+    console.log(i)
+   }
+}
+
+// let vocales='aeiou'
+
+// console.log(vocales.length)
+
+// for (let i=1 ;i<=20;i++){
+//     console.log(vocales)
+// }
+
+
+let texto='¿hola como te llamas luciana?'
+let contador=0
+for (let i=0 ;i<texto.length;i++){
+   if(texto[i]=='a'){
+    contador+=1
+    switch(texto[i]){
+        case 'a': case 'e' : case 'i' : case 'o' : 'u'
+        contador+=
+    }
+     }
+    }
+
+   console.log('tienes', contador, 'vocales,a')
+
+   // hacer un programa que me muestre el factorial de un numero.
+   51=5*4*3*2*1
+   51=120
+
+   
+   // hacer un programa que nos determine si un texto es pilmora
+   ada
+
+   // hacer un ´programa que muestre los n numeros de la serie 
+
+   1-2-3-5-8-13-23-34
---
+++ 7.funciones/ejercicios.js
@@ -0,0 +1,7 @@
+// averiguar concepto:
+
+// FUNCIONES CONCEPTO
+
+// Las funciones son uno de los bloques de construcción fundamentales en JavaScript. Una función en JavaScript es similar a un procedimiento
+//  un conjunto de instrucciones que realiza una tarea o calcula un valor, pero para que un procedimiento califique como función, debe tomar
+//  alguna entrada y devolver una salida donde hay alguna relación obvia entre la entrada y la Salida. 
