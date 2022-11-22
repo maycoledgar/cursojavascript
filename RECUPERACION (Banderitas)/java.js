f9e6b2cc612fc5cc80e53e840761e9978992dde9
maycol <maycoledgar04@gmail.com>
2022-11-21 20:13:26 -0500

    recuperacion (banderitas)

---
+++ RECUPERACION (Banderitas)/java.js
@@ -0,0 +1,24 @@
+
+const espa = document.createElement("div")
+espa.classList.add = "s"
+function boton1(){
+    img1 = "<img src='imagenes/colombia.JPG' alt=''/> "
+    document.getElementById("espacio").innerHTML = img1;
+
+}
+function boton2(){
+    img2 = '<img src="imagenes/peru.jpg" alt=""/>'
+    document.getElementById("espacio").innerHTML = img2;
+}
+function boton3(){
+    img3 = '<img src="imagenes/rusia.jpg" alt=""/>'
+    document.getElementById("espacio").innerHTML = img3;
+}
+function boton4(){
+    img4 = '<img src="imagenes/mexico.jpg" alt=""/>'
+    document.getElementById("espacio").innerHTML = img4;
+}
+function boton5(){
+    img5 = '<img src="imagenes/wakanda.jpg" alt=""/>'
+    document.getElementById("espacio").innerHTML = img5;
+}
